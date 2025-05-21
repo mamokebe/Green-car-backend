c251dea18dd7fffa6619c8729b00662fad90eda5
Kebede B. <140273706+mamokebe@users.noreply.github.com>
2025-05-21 14:22:43 -0500

    Update addressRoute.js

--- routes/addressRoute.js
+++ routes/addressRoute.js
@@ -5,10 +5,10 @@ import authUser from '../middlewares/authUser.js';
 const addressRouter = express.Router();
 
 // Add Address
-addressRouter.post('/add', authUser, addAddress); // Add a new address
+addressRouter.post('/add',  addAddress); // Add a new address
 // Get Address by User ID
-addressRouter.get('/get', authUser, getAddress); // Get address by user ID
+addressRouter.get('/get', getAddress); // Get address by user ID
 // Delete Address by ID
-addressRouter.delete('/:id', authUser, deleteAddress); // Delete address by ID
+addressRouter.delete('/:id',  deleteAddress); // Delete address by ID
 // Update Address by ID
 export default addressRouter;
